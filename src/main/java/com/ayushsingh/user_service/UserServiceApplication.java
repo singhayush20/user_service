c3183032393f434e833df2314c6739b211805447
singhayush20 <ayushsingh20november@gmail.com>
2022-12-26 20:04:45 +0530

    Eureka Client Configuration

--- src/main/java/com/ayushsingh/user_service/UserServiceApplication.java
+++ src/main/java/com/ayushsingh/user_service/UserServiceApplication.java
@@ -4,9 +4,12 @@ package com.ayushsingh.user_service;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+// @EnableEurekaClient //not working
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
